6745ec39fe7f911673090b4a6b09c17bf8cbb244
Alex <alex.hhooker@gmail.com>
2018-08-27 17:12:18 +0200

    Update vectorized_operations.py

--- vectorized_operations.py
+++ vectorized_operations.py
@@ -17,3 +17,25 @@ def matrix_cosine_sims(M1, M2):
     denominators = norm1_tiled * norm2_tiled
     
     return dp_mat / denominators
+
+# N.B. these are assuming data with batch, len, feats dimensions (i.e. sequential data)
+def np_cross_entropy(targets, preds, epsilon=1e-12):
+    # at each nucleotide we want to effectively dot product the target aa vector and the log probs
+    preds = np.clip(preds, epsilon, 1-epsilon)
+    masked_probs = targets * np.log(preds)
+    return -np.sum(masked_probs, axis=(1,2))
+
+def np_elbo(z_mean, z_var, targets, preds, log_var=False, epsilon=1e-12):
+    return np_kl_loss(z_mean, z_var) + np_cross_entropy(targets, preds)
+
+def np_kl_loss(z_mean, z_var):
+    return - 0.5 * np.sum(1 + np.log(z_var+1e-8) - z_var - np.square(z_mean), axis=-1)
+
+# def np_logvar_kl
+# TODO define logvar loss to be used in delta_elbo
+
+def np_klstd_loss(z_var):
+    return - 0.5 * np.sum(1 + np.log(z_var + 1e-8) - z_var, axis=-1)
+
+def np_klmu_loss(z_mean):
+    return 0.5 * np.sum(np.square(z_mean), axis=-1)
